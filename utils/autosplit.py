dc531152c1e747d2e9d6c2d1b159fd23456d0b23
Mohsen M <me@bumbleboss.xyz>
2023-12-16 16:14:15 +0200

    feat: added arguments for autosplit

--- utils/autosplit.py
+++ utils/autosplit.py
@@ -1,60 +1,75 @@
 import os
 import math
+import argparse
 
+def get_weighted_list(images_dir, weights):
+  img_arr = os.listdir(images_dir)
+  img_len = len(img_arr)
 
-def get_weighted_list(images_dir, weights=(0.9, 0.1, 0.0)):
-    img_arr = os.listdir(images_dir)
-    img_len = len(img_arr)
+  train_i = math.floor(img_len * weights[0])
+  valid_i = math.floor(img_len * weights[1])
+  test_i = math.floor(img_len * weights[2])
 
-    train_i = math.floor(img_len * weights[0])
-    valid_i = math.floor(img_len * weights[1])
-    test_i = math.floor(img_len * weights[2])
+  print('Used %d out of %d images' % (train_i + valid_i + test_i, img_len))
 
-    print('Used %d out of %d images' % (train_i + valid_i + test_i, img_len))
-
-    return [
-        img_arr[:train_i],
-        img_arr[train_i:train_i + valid_i],
-        img_arr[train_i + valid_i:train_i + valid_i + test_i]
-    ]
-
+  return [
+    img_arr[:train_i],
+    img_arr[train_i:train_i + valid_i],
+    img_arr[train_i + valid_i:train_i + valid_i + test_i]
+  ]
 
 def create_dir(dataset_dir, category_dir, subset_dir):
-    for category in category_dir:
-        for subset in subset_dir:
-            computed_path = os.path.join(dataset_dir, category, subset)
-            if not os.path.isdir(computed_path):
-                os.makedirs(computed_path)
-                print('Directory does not exist, creating directory: %s' % computed_path)
+  for category in category_dir:
+    for subset in subset_dir:
+      computed_path = os.path.join(dataset_dir, category, subset)
 
-
-dataset = 'Self-Driving-Car-3'
-images_d = 'export/images'
-labels_d = 'export/labels'
-category_d = ['images', 'labels']
-subset_d = ['train', 'valid', 'test']
-
+      if not os.path.isdir(computed_path):
+        os.makedirs(computed_path)
+        print('Directory does not exist, creating directory: %s' % computed_path)
 
 def main():
-    # Create directories if they don't exist
-    create_dir(dataset, category_d, subset_d)
+  weights = (0.9, 0.1, 0.0)
+  parser = argparse.ArgumentParser(description='Autosplit images')
 
-    # Create weighted list of images by providing its directory
-    weighted_list = get_weighted_list(os.path.join(dataset, images_d), (0.6, 0.2, 0.2))
+  parser.add_argument('-d', '--dataset', help='dataset directory name', type=str, required=True)
+  parser.add_argument('-i', '--image', help='relative images directory path', type=str, required=True)
+  parser.add_argument('-l', '--label', help='relative labels directory path', type=str, required=True)
+  parser.add_argument('-y', '--yolo', help='yolo version', type=int, required=True)
+  parser.add_argument('-w', '--weights', help='weight distribution', nargs='+', type=float, default=weights)
 
-    for i, category in enumerate(weighted_list):
-        for file in category:
-            org_img = os.path.join(dataset, images_d, file)
-            org_txt = os.path.join(dataset, labels_d, file)
-            org_txt = os.path.splitext(org_txt)[0]+'.txt'
+  args = parser.parse_args()
 
-            image_path = os.path.join(dataset, category_d[0], subset_d[i], file)
-            txt_path = os.path.join(dataset, category_d[1], subset_d[i], file)
-            txt_path = os.path.splitext(txt_path)[0]+'.txt'
+  dataset = args.dataset
+  images_d = args.image
+  labels_d = args.label
+  weights = tuple(args.weights)
 
-            os.popen('cp %s %s' % (org_img, image_path))
-            os.popen('cp %s %s' % (org_txt, txt_path))
+  if (args.yolo == 7):
+    category_d = ['train', 'valid', 'test']
+    subset_d = ['images', 'labels']
+  else:
+    category_d = ['images', 'labels']
+    subset_d = ['train', 'valid', 'test']
+    
+  # Create directories if they don't exist
+  create_dir(dataset, category_d, subset_d)
+
+  # Create weighted list of images by providing its directory
+  weighted_list = get_weighted_list(os.path.join(dataset, images_d), weights)
+
+  for i, category in enumerate(weighted_list):
+    for file in category:
+      org_img = os.path.join(dataset, images_d, file)
+      org_txt = os.path.join(dataset, labels_d, file)
+      org_txt = os.path.splitext(org_txt)[0]+'.txt'
+
+      image_path = os.path.join(dataset, category_d[0], subset_d[i], file)
+      txt_path = os.path.join(dataset, category_d[1], subset_d[i], file)
+      txt_path = os.path.splitext(txt_path)[0]+'.txt'
+
+      os.popen('cp %s %s' % (org_img, image_path))
+      os.popen('cp %s %s' % (org_txt, txt_path))
 
 
 if __name__ == "__main__":
-    main()
+  main()
