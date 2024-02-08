0494a1bd6aaa6ddd4f55c733d8e904661cca211e
Mohsen M <me@bumbleboss.xyz>
2024-02-08 02:36:54 +0200

    feat: added pid and switch cases for modes - still wip

--- front_board/front_board.h
+++ front_board/front_board.h
@@ -12,12 +12,20 @@
 #define CAN_PIN 4
 #define PEDAL_PIN A6
 
+#define ACC_LED 18
+
 /**********************************
  * DEFINE DATA TYPES AND STRUCT
 ***********************************/
 
 typedef unsigned char uint8;    // 255
 typedef unsigned long uint32;   // 4,294,967,295
+
+typedef enum {
+  PEDAL_MODE,
+  PID_MODE,
+  AUTONOMOUS_MODE
+} CAR_MODES;
 
 struct can_frame can_msg_send;
 struct can_frame can_msg_receive;
--- front_board/front_board.ino
+++ front_board/front_board.ino
@@ -1,12 +1,34 @@
 #include <mcp2515.h>
+#include <PID_v1.h>
+#include <Wire.h>
+#include <SPI.h>
 #include "front_board.h"
 
 MCP2515 mcp2515(CAN_PIN);
 
-uint8 pedal_value = 0;
-uint32 displacement = 0;
-uint32 speed = 0;
+uint8 pedal_value;
+uint32 displacement;
+uint32 speed;
 
+CAR_MODES car_mode;
+
+byte I2C_B1, I2C_B2;
+
+bool ACC_FLAG, ACC_LOOP;
+bool ACC_SW;
+
+
+// PID parameters
+double input_speed;
+double desired_speed = 0.01;
+double pid_output;
+double K_P = 50;
+double K_I = 5;
+double K_D = 0.5;
+
+double throttle_output;
+
+PID pid_control(&input_speed, &pid_output, &desired_speed, K_P, K_I, K_D, DIRECT);
 
 void setup() {
   can_msg_send.can_id = 0x00;
@@ -14,10 +36,15 @@ void setup() {
   can_msg_send.data[0] = 0x00; // throttle
   can_msg_send.data[1] = 0x00; // bulb
 
+  Wire.begin(8); // join I2C bus with address #8
+  Wire.onReceive(I2C_Read); // register event
+
   Serial.begin(115200);
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS);
   mcp2515.setNormalMode();
+
+  pid_control.SetMode(AUTOMATIC);
 }
 
 void loop() {
@@ -29,26 +56,56 @@ void loop() {
   // map pedal resolution to motor
   pedal_value = map(analogRead(PEDAL_PIN), 0, 1024, 0, 255);
 
-  // we only compute float values for printing
-  Serial.print("Displacement: ");
-  Serial.print(displacement / 1000.0, 2);
-  Serial.print("\n");
+  // convert speed to appropriate m/s double
+  input_speed = speed * (1000 / 100000000.0);
 
-  Serial.print("Speed: ");
-  Serial.print(speed * (1000 / 100000000.0), 2);
-  Serial.print("\n");
+  if (ACC_SW == 1 && ACC_FLAG == 1) {
+    delay(50);
 
-  Serial.print("Pedal: ");
-  Serial.print(pedal_value);
-  Serial.print("\n");
+    if (ACC_SW == 1) {
+      ACC_LOOP = !ACC_LOOP;
+      ACC_FLAG = 0;
+    }
+  }
 
-  // set bulb depending on settings
-  bitWrite(can_msg_send.data[1], 0, 1); // left bulb
-  bitWrite(can_msg_send.data[1], 1, 0); // right bulb
-  bitWrite(can_msg_send.data[1], 2, 1); // brake bulb
+  if (ACC_SW == 0) {
+    ACC_FLAG = 1;
+    car_mode = PID_MODE;
+  } else {
+    car_mode = PEDAL_MODE;
+  }
 
-  // send pedal as throttle
-  can_msg_send.data[0] = pedal_value;
+  digitalWrite(ACC_LED, ACC_LOOP);
+
+
+  switch (car_mode) {
+    case (PEDAL_MODE):
+      bitWrite(can_msg_send.data[1], 0, 1);
+      bitWrite(can_msg_send.data[1], 1, 0);
+      throttle_output = pedal_value;
+      break;
+    case (PID_MODE):
+      bitWrite(can_msg_send.data[1], 0, 1);
+      bitWrite(can_msg_send.data[1], 1, 1);
+
+      // replaced later with variable input
+      desired_speed = 1;
+
+      pid_control.Compute();
+      throttle_output = pid_output;
+      break;
+    default:
+      throttle_output = pedal_value;
+      break;
+  }
 
   mcp2515.sendMessage(&can_msg_send);
 }
+
+void I2C_Read(int howMany) {
+  I2C_B1 = Wire.read();
+  I2C_B2 = Wire.read();
+
+  ACC_SW = bitRead(I2C_B2, 2);
+}
+
