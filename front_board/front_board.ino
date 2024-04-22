35764bae09e53bcb4427702d6cc4f559d7eff560
Abdulrahman Alrashidi <da7oomo2000@gmail.com>
2024-04-22 20:14:51 +0200

    feat : updated maping values

--- front_board/front_board.ino
+++ front_board/front_board.ino
@@ -295,5 +295,5 @@ void steering_limit_interrupt() {
 void ackerman_callback(const ackermann_msgs::AckermannDrive& ackerman_data) {
   angle_value = map(ackerman_data.steering_angle * 100, STEERING_MAX_ANGLE * -100, STEERING_MAX_ANGLE * 100, -1 * STEERING_MAX_STEPS, STEERING_MAX_STEPS);
   angle_value = constrain(angle_value, -1 * STEERING_MAX_STEPS, STEERING_MAX_STEPS);
-  throttle_value = map(ackerman_data.speed * 1000, 0, 1000, 240, 1023);
+  throttle_value = map(ackerman_data.speed * 1000, 0, 1000, 230, 1023);
 }
