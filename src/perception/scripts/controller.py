6a49d20afc6b38328af6b2431979aa0764eb7f46
Mohsen M <me@bumbleboss.xyz>
2024-04-24 11:34:44 +0200

    test: revised logic for overriding 0.0 speed

--- src/perception/scripts/controller.py
+++ src/perception/scripts/controller.py
@@ -7,22 +7,21 @@ from zed_interfaces.msg import ObjectsStamped
 
 stop_car = False
 object_limit = 5
-previous_speed
+previous_speed = None
 
 def ackermann_callback(msg: AckermannDrive, cmd_pub):
     global stop_car
     global previous_speed
 
-    previous_time = time.time()
-
     if stop_car:
-        msg.speed = 0
+        msg.speed = 0.0
     
-    if not stop_car and msg.speed != 0.0:
-        previous_speed = msg.speed
+    if not stop_car:
+        if msg.speed != 0.0:
+            previous_speed = msg.speed
 
-    if not stop_car and msg.speed == 0.0 and previous_speed is not None:
-        msg.speed = previous_speed
+        elif msg.speed == 0.0 and previous_speed not None:
+            msg.speed = previous_speed
 
     cmd_pub.publish(msg)
 
