f8a814aa70b15a8c9c3457e6ba638eb97a4bea17
Dinafouad8 <dinaf8383@gmail.com>
2024-04-25 23:50:18 +0200

    Add Ultrasonic JSN-SR04T code for UART mode

---
+++ src/board_controller/ultrasonic/new_ultrasonic.ino
@@ -0,0 +1,36 @@
+//Tasted code separately on Arduino Uno
+#include <Wire.h>
+#include <Ultrasonic.h>
+
+const int rxPin = 2;  // Rx pin of the ultrasonic sensor
+const int txPin = 3;  // Tx pin of the ultrasonic sensor
+
+Ultrasonic ultrasonic(rxPin, txPin);
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(8);                // Set I2C address to 8
+  Wire.onRequest(requestEvent); // Register the function to handle I2C requests
+}
+
+void loop() {
+  // Reading data from the ultrasonic sensor
+  int distance = ultrasonic.read();
+
+  // Print the distance to the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500);
+}
+
+void requestEvent() {
+  // Reading data from the ultrasonic sensor
+  int distance = ultrasonic.read();
+
+  // Sending the distance data over I2C
+  Wire.write(distance >> 8);  // Send the high byte
+  Wire.write(distance & 0xFF); // Send the low byte
+
+
