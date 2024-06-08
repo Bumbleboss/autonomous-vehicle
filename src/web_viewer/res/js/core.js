9b5fb31baaba193461043f816452aaf7a79a3d56
Mohsen M <me@bumbleboss.xyz>
2024-06-08 11:04:25 +0300

    feat: added listeners to other related fields

--- src/web_viewer/res/js/core.js
+++ src/web_viewer/res/js/core.js
@@ -3,26 +3,25 @@ const ros = new ROSLIB.Ros({
   url: 'ws://localhost:9090'  // Adjust the WebSocket URL as needed
 });
 
+const speed_elm = document.getElementById('speed');
 const drive_elm = document.getElementById('mode');
 const connect_elm = document.getElementById('connect');
 const calibrate_elm = document.getElementById('calibrate');
+const objs_elm = document.getElementById('objects');
 
 // event listeners for connection status
 ros.on('connection', () => {
   connect_elm.classList.remove('inactive');
-  connect_elm.classList.remove('pending');
   connect_elm.classList.add('active');
 });
 
 ros.on('error', (error) => {
-  connect_elm.classList.add('inactive');
-  connect_elm.classList.remove('pending');
+  connect_elm.classList.remove('inactive');
   connect_elm.classList.remove('active');
 });
 
 ros.on('close', () => {
-  connect_elm.classList.remove('inactive');
-  connect_elm.classList.add('pending');
+  connect_elm.classList.add('inactive');
   connect_elm.classList.remove('active');
 });
 
@@ -31,6 +30,19 @@ const driving_listener = new ROSLIB.Topic({
   ros: ros,
   name: '/driving_mode',
   messageType: 'std_msgs/UInt8'
+});
+
+const odom_listener = new ROSLIB.Topic({
+  ros: ros,
+  name: '/zed/zed_node/odom',
+  messageType: 'nav_msgs/Odometry'
+});
+
+
+const obj_listener = new ROSLIB.Topic({
+  ros: ros,
+  name: '/zed2/zed_node/obj_det/objects',
+  messageType: 'zed_interfaces/ObjectsStamped'
 });
 
 // driving modes
@@ -50,3 +62,13 @@ driving_listener.subscribe((msg) => {
     calibrate_elm.classList.add('active');
   }
 });
+
+odom_listener.subscribe((msg) => {
+  let vel = msg.twist.twist.linear.z;
+  speed_elm.children[0].innerText = `${vel} m/s`
+})
+
+obj_listener.subscribe((msg) => {
+  let objs = msg.objects.length;
+  objs_elm.children[0].innerText = objs
+})
