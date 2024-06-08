7da1b9ff5d84c984572f2646f0724d9f1b78c4fd
Mohsen M <me@bumbleboss.xyz>
2024-06-08 10:39:47 +0300

    feat: connection status is now functional - calibration and driivng mode now functional - fixed script not added at the end

--- src/web_viewer/res/js/core.js
+++ src/web_viewer/res/js/core.js
@@ -3,37 +3,50 @@ const ros = new ROSLIB.Ros({
   url: 'ws://localhost:9090'  // Adjust the WebSocket URL as needed
 });
 
-// event listeners
+const drive_elm = document.getElementById('mode');
+const connect_elm = document.getElementById('connect');
+const calibrate_elm = document.getElementById('calibrate');
+
+// event listeners for connection status
 ros.on('connection', () => {
-  console.log('Connected to websocket server.');
+  connect_elm.classList.remove('inactive');
+  connect_elm.classList.remove('pending');
+  connect_elm.classList.add('active');
 });
 
 ros.on('error', (error) => {
-  console.log('Error connecting to websocket server: ', error);
+  connect_elm.classList.add('inactive');
+  connect_elm.classList.remove('pending');
+  connect_elm.classList.remove('active');
 });
 
-ros.on('close', function() {
-  console.log('Connection to websocket server closed.');
+ros.on('close', () => {
+  connect_elm.classList.remove('inactive');
+  connect_elm.classList.add('pending');
+  connect_elm.classList.remove('active');
 });
 
-// Create a ROS topic subscriber
-var drivingModeListener = new ROSLIB.Topic({
+// driving mode and calibration subscriber
+const driving_listener = new ROSLIB.Topic({
   ros: ros,
   name: '/driving_mode',
   messageType: 'std_msgs/UInt8'
 });
 
-// Map the UInt8 values to driving modes
-var drivingModes = {
+// driving modes
+const drivingModes = {
   0: 'Manual Mode',
   1: 'Calibration Mode',
   2: 'Autonomous Mode',
   3: 'Steady State Mode'
 };
 
-// // Subscribe to the topic
-// drivingModeListener.subscribe(function(message) {
-//   var mode = drivingModes[message.data] || 'Unknown Mode';
-//   document.getElementById('driving-mode').innerText = mode;
-//   console.log('Received message on /driving_mode: ' + message.data);
-// });
+// update driving mode
+driving_listener.subscribe((msg) => {
+  drive_elm.children[0].innerText = drivingModes[msg.data];
+  
+  if (msg.data == 1) {
+    calibrate_elm.classList.remove('pending');
+    calibrate_elm.classList.add('active');
+  }
+});
